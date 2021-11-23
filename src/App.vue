b1115edb608e8a4069579e52147c65cf9dbe4559
Mdigennaro <michele.digennaro719@gmail.com>
2021-11-23 16:00:45 +0100

    creazione titolo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
-  
+
+  <div id="app">
+    
+    <Title />
+    <main>M</main>
+    <footer>F</footer>
+
+  </div>
+
 </template>
 
 <script>
+import Title from "./components/Title.vue"
 
 export default {
   name: 'App',
   components: {
+    Title
   }
 }
 </script>
 
 <style>
+
+#app{
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  text-align: center;
+}
+
 </style>
---
+++ src/components/Title.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <header>
+  <h1>La mia prima app con Vue CLI!</h1>
+  </header>
+
+</template>
+
+<script>
+export default {
+  name: "Title"
+}
+</script>
+
+<style>
+
+h1{
+  color: blue;
+  text-transform: uppercase;
+  font-weight: bold;
+  text-decoration: underline;
+}
+
+</style>
